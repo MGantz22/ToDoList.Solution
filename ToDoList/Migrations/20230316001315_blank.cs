bf02ed805874f29f52be4e42b30980dc04dae0dc
Mitchell Gantz <mgantz22@icloud.com>
2023-03-15 17:14:04 -0700

    Update Items.cs model

---
+++ ToDoList/Migrations/20230316001315_blank.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class blank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fixshit",
+                table: "Items");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Fixshit",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
